424f025c1b9988e725282a5360b7da5834526963
DavidBorsky <dborsky@icloud.com>
2025-09-18 14:11:36 -0400

    assignment
    first

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,3 +3,11 @@ public class Assignment1Program1 {
 
   }
 }
+For the first program, you’ll be writing a program that creates a string based on temperature and random factors.
+
+Your program should print your first and last initials in large block letters. Use each letter’s corresponding characters to form the letters. (See the Example Output section below.)
+Now, take input from the user in the form of a 5-character string, then save the string in reverse without the first and last characters. In other words, trim the outside characters. For example, if the user enters abewz, your program would save web.
+Next, ask the user to enter a number in Fahrenheit. Your program should convert the value to Celsius and save it. For example, entering 32 should yield 0.0.
+In this penultimate step, generate and save a random number between 32 – 16,384, inclusive.
+And, finally, combine the results of steps 2 – 4 and print it to the screen.
+
